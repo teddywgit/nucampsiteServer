ed678d42eac2a338ce1b89f1c619ba2f7cef7dee
Teddy Wylupski <ted.wylupski@gmail.com>
2021-01-22 17:17:57 -0800

    OAuth Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '734582597220935',
+        clientSecret: '0714a3190c286e922fd37e2d31ea15ed'
+    }
 }
